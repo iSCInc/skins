673aa75b595c02f1748636a4926aa47a2e88b0d9
Suriyaa Kudo <isc.suriyaa@gmail.com>
2015-03-14 16:40:55 +0100

    Upload Trueglass

---
+++ iscinc/Splash/Splash.i18n.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Internationalization file for the Splash skin or some such...
+ *
+ * @file
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'splash-skin-desc' => 'Splash skin for MediaWiki, as designed for zaori.org',
+	'site-banner' => '{{SITENAME}}',
+);
+
+/** Message documentation (Message documentation) */
+$messages['qqq'] = array(
+	'splash-skin-desc' => '{{desc|name=Splash}}',
+	'site-banner' => 'Used as the banner text for the site',
+);
